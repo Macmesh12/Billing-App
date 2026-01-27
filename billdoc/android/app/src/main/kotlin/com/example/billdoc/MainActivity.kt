894218e8fc4a7e77c558a83498bec416dff427ef
macmesh12 <mesham736@gmail.com>
2026-01-27 02:45:20 +0000

    flutter initiatited

---
+++ billdoc/android/app/src/main/kotlin/com/example/billdoc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.billdoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ billdoc/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'screens/home_screen.dart';
+import 'providers/app_state.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AppState(),
+      child: const BillingApp(),
+    ),
+  );
+}
+
+class BillingApp extends StatelessWidget {
+  const BillingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BillDoc',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        scaffoldBackgroundColor: const Color(0xFFF9FAFB),
+        textTheme: GoogleFonts.interTextTheme(),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFFEAB308),
+          primary: const Color(0xFFEAB308),
+          secondary: const Color(0xFFFBBF24),
+        ),
+        cardTheme: const CardTheme(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
